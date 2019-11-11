55f4b130cfce6d0d454eb3df42e50deb80c69f27
Glanze25 <Glanze25@users.noreply.github.com>
2019-11-11 13:47:29 +0100

    Grundrechenarten

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 9;
+	int b = 5;
+
+	System.out.println("a = " + a + "; b = " + b);
+	System.out.println(a + " + " + b + " = " + (a + b));
+	System.out.println(a + " - " + b + " = " + (a-b));
+	System.out.println(a + " * " + b + " = " + (a*b));
+    System.out.println(a + " / " + b + " = " + (a/b));
+    System.out.println(a + " % " + b + " = " + (a%b));
+    }
+}

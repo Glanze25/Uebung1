73f8d9f80ab0eb53fd51485672e861cf57f6d796
Glanze25 <Glanze25@users.noreply.github.com>
2019-11-11 14:16:21 +0100

    Übung1 Ende

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,15 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	int a = 9;
-	int b = 5;
+	public static void main(String[] args) {
+		int a = 9;
+		int b = 5;
 
-	System.out.println("a = " + a + "; b = " + b);
-	System.out.println(a + " + " + b + " = " + (a + b));
-	System.out.println(a + " - " + b + " = " + (a-b));
-	System.out.println(a + " * " + b + " = " + (a*b));
-    System.out.println(a + " / " + b + " = " + (a/b));
-    System.out.println(a + " % " + b + " = " + (a%b));
-    }
+		System.out.println("a = " + a + "; b = " + b);
+		System.out.println(a + " + " + b + " = " + (a + b));
+		System.out.println(a + " - " + b + " = " + (a - b));
+		System.out.println(a + " * " + b + " = " + (a * b));
+		System.out.println(a + " / " + b + " = " + (a / b));
+		System.out.println(a + " % " + b + " = " + (a % b));
+	}
 }
